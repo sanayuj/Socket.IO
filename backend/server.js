f35db44d02862eab12311881d408ff331c358830
sanayuj <sanayuj2255@gmail.com>
2024-02-28 15:22:57 +0530

    formatted code

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
-const express=require("express")
-const app=express()
-const server=require("http").createServer(app)
-const io=require("socket.io")(server,{cors: {
-    origin: "*"
-  }})
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log("Socket is this :", socket);
 
-io.on("connection",(socket)=>{
-    console.log("Socket is this :",socket);
+  socket.on("chat", (payload) => {
+    console.log("Payload is this : ", payload);
+    io.emit("chat", payload);
+  });
+});
 
-    socket.on("chat",(payload)=>{
-        console.log("Payload is this : ",payload);
-        io.emit("chat",payload)
-    })
-})
-
-
-
-
-server.listen(4000,()=>{
-    console.log("Server is active...")
-})
+server.listen(4000, () => {
+  console.log("Server is active...");
+});
