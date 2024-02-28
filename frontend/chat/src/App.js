75cf597bf169d6ff344f21a9657598a0c94e69fc
sanayuj <sanayuj2255@gmail.com>
2024-02-28 15:18:49 +0530

    formatted code

--- frontend/chat/src/App.js
+++ frontend/chat/src/App.js
@@ -1,33 +1,36 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 
 const socket = io.connect("http://localhost:4000");
 
-const userName=nanoid(4)
+const userName = nanoid(4);
 
 function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
 
-  const sendChat=(e)=>{
-    e.preventDefault()
-    socket.emit("chat",{message,userName})
-    setMessage("")
-  }
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { message, userName });
+    setMessage("");
+  };
 
-  useEffect(()=>{
-    socket.on("chat",(payload)=>{
-      setChat([...chat,payload])
-    })
-  })
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
   return (
     <div className="App">
       <header className="App-header">
         <h1>Chat </h1>
-        {chat.map((value,index)=>(
-          <p key={index}>{value.message}<span className="userName"> id:{value.userName}</span></p>
+        {chat.map((value, index) => (
+          <p key={index}>
+            {value.message}
+            <span className="userName"> id:{value.userName}</span>
+          </p>
         ))}
         <form onSubmit={sendChat}>
           <input
