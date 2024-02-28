5ce4bdabef1d2b93239d6a1f8bbee383322c2917
sanayuj <sanayuj2255@gmail.com>
2024-02-28 14:58:22 +0530

    send msg and displayed msg

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 const express=require("express")
 const app=express()
-
 const server=require("http").createServer(app)
-
-
-const io=require("socket.io")(server)
+const io=require("socket.io")(server,{cors: {
+    origin: "*"
+  }})
 
 
 io.on("connection",(socket)=>{
-    console.log("Socket",socket);
+    console.log("Socket is this :",socket);
+
+    socket.on("chat",(payload)=>{
+        console.log("Payload is this : ",payload);
+        io.emit("chat",payload)
+    })
 })
 
-io.on("chat",(payload)=>{
-    console.log("Payload: ",payload);
-    io.emit("chat",payload)
-})
+
 
 
 server.listen(4000,()=>{
--- frontend/chat/src/App.js
+++ frontend/chat/src/App.js
@@ -1,16 +1,44 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import nanoid from "nanoid";
 
-import './App.css';
-import { useState,useEffect } from 'react';
-import io from "socket.io-client"
-import nanoid from "nanoid"
-
-const socket=io.connect("http://localhost:4000")
+const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChat=(e)=>{
+    e.preventDefault()
+    socket.emit("chat",{message})
+    setMessage("")
+  }
+
+  useEffect(()=>{
+    socket.on("chat",(payload)=>{
+      setChat([...chat,payload])
+    })
+  })
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Chat </h1>
+        <h1>Chat </h1>
+        {chat.map((value,index)=>(
+          <p key={index}>{value.message}</p>
+        ))}
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send message"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button>Send</button>
+        </form>
       </header>
     </div>
   );
